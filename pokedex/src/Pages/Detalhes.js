a4a2624f90e06727362ef7519d02661e2efc2c9b
Matheus_Jesuino <matheusjesuino96@hotmail.com>
2022-05-03 16:57:23 -0300

    Feito o esqueleto e as Rotas do site

--- pokedex/src/Pages/Detalhes.js
+++ pokedex/src/Pages/Detalhes.js
@@ -1,13 +1,45 @@
 import React from "react";
-import { goToDetalhes, goToHomePage, goToPokedex } from "../Coordinator/Coordinator"
-import {useNavigate } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import styled from "styled-components";
+import { useEffect, useState, useNavigate } from "react";
+import axios from "axios";
+import { goToDetalhes, goToPokedex, goToHomePage } from "../Coordinator/Coordinator"
+import { BASE_URL } from "../Contants/contants";
 
 export const Detalhes = () => {
     const navigate = useNavigate()
 
+    const [getDetalhes, setGetDetalhes] = useState([])
+
+    useEffect(() => {
+        detalhesPokemon()
+    }, [])
+
+    const detalhesPokemon = () => {
+        axios
+            .get(`${BASE_URL}/{id ou nome}/`)
+            .then((res) => {
+                console.log(res.data.id)
+                setGetDetalhes(res.data.id)
+            })
+            .catch((err) => {
+                alert(err.data.id)
+            })
+    }
+ const detalhes = getDetalhes.map((detalhe)=>{
+return(
+    <div key={detalhe.id} >
+        {/* <li>Poderes : {detalhe.planet}</li> */}
+        <li>Principais Abilidades : {detalhe.abilities}</li>
+        <li>Tipo : {detalhe.species}</li>
+    </div>
+)
+ })
+
     return (
         <div>
             <h1>Detalhes do Pokemon</h1>
+            {detalhes}
             <button onClick = {()=> goToPokedex(navigate)}>Ir a Pokédex</button>
             <button onClick={()=> goToHomePage(navigate) }>Voltar</button>
           
